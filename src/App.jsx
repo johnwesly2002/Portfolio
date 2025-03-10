f5e5a752cdf9bce7afbe6aa8777a1058a8350b05
johnwesly2002 <ujohnwesly8@gmail.com>
2025-03-11 03:16:47 +0530

    contact button change

--- src/App.jsx
+++ src/App.jsx
@@ -444,7 +444,7 @@ const handleEducationTabClick = (index) => {
       <h2 className='Contactheading'>Looks Interesting?</h2>
       <h2 className='ContactTitle'>Get In Touch</h2>
       <p>I’m currently looking for any new interesting opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!</p>
-      <a href='mailto:ujohnwesly8@gmail.com' target='_blank' className='Hire_button'>Hire Me</a>
+      <a href='mailto:ujohnwesly8@gmail.com' target='_blank' className='Contact_button'>Hire Me</a>
       </section>
     </main>
     <footer>
