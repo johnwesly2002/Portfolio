03907a61958548915c0a257f740d468b63b42f60
johnwesly2002 <ujohnwesly8@gmail.com>
2025-08-16 18:17:26 +0530

    changes

--- src/components/footerComponent/Footer.jsx
+++ src/components/footerComponent/Footer.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "../../App.css";
 import footerImg from '../../assets/Ani1.json';
 import Lottie from 'react-lottie';
+import ShinyText from '../constants/shinyTextComponent/ShinyText';
 const Footer = () => {
   return (
       <footer>
-      <p>Designed and Made By&nbsp;</p>
-      <p>John wesly Uchula</p>
+      {/* <p>Designed and Made By&nbsp;</p>
+      <p>John wesly Uchula</p> */}
+      <ShinyText text="Designed and Made By John wesly Uchula" disabled={false} speed={6} className='text' />
       <div className='lottie'>
       <Lottie options={{
          loop: true,           
